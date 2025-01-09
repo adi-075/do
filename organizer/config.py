962874616983ff66505ef03ae9811d256937d000
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-09 16:39:43 -0500

    Create a config file

---
+++ organizer/config.py
@@ -0,0 +1,8 @@
+# Use this file for configuration settings
+FILE_CATEGORIES = {
+    "Images": ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "svg"],
+    "Videos": ["mp4", "avi", "mov", "flv", "wmv", "mkv"],
+    "Documents": ["doc", "docx", "pdf", "txt", "rtf"],
+    "Audio": ["mp3", "wav"],
+    "Miscellaneous": []  # All the other files go in this directory
+}
