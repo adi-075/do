bab55054cc7db63656ce877ba594b79554be367e
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-09 19:06:00 -0500

    Update config.py

--- organizer/config.py
+++ organizer/config.py
@@ -1,8 +1,9 @@
 # Use this file for configuration settings
 FILE_CATEGORIES = {
-    "Images": ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "svg"],
-    "Videos": ["mp4", "avi", "mov", "flv", "wmv", "mkv"],
-    "Documents": ["doc", "docx", "pdf", "txt", "rtf"],
-    "Audio": ["mp3", "wav"],
-    "Miscellaneous": []  # All the other files go in this directory
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".svg", ".heic"],
+    "Videos": [".mp4", ".avi", ".mov", ".flv", ".wmv", ".mkv"],
+    "Documents": [".doc", ".docx", ".pdf", ".txt", ".rtf"],
+    "Audio": [".mp3", ".wav"],
+    "Executables": [".exe", ".msi", ".dmg", ".deb"],
+    # "Fonts": ["ttf", "otf", "fon"],
 }
